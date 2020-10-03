a906cfc40707dc90b23b26a7681b36142287863b
Diana <diana.suhoverhova@mail.ru>
2020-10-03 22:24:04 +0300

    Changes in the class and Thomas_algorithm
    Add docstring and exceptions to the class and Thomas_algorithm,
    and change the name of class (it is CamelCase now).

--- cyclic_interpolate.py
+++ cyclic_interpolate.py
@@ -1,18 +1,35 @@
 import numpy as np
-import math
 
 accuracy = 1e-14
 
 
-class cyclic_interp_curve:
-    '''
-    Class for cyclic curve
-    Methods:
-        1) hermit_cubic_spline - makes up a spline function and counts the value of cubic spline in Hermit form in t
-        2) draw - makes a scatter plot of initial set of dots and
-           a curve that makes up from interpolate functions
-    '''
+class CyclicInterpCurve:
+    """
+    Cubic interpolating function in Hermite form with periodic
+    boundary conditions.
 
+    Parameters
+    ----------
+        x : 1-D array, shape (k,)
+            Values of x - coordinate of a given set of points.
+        y : 1-D array, shape (k,)
+            Values of y - coordinate of a given set of points.
+        der : 1-D array, shape (k,)
+            First derivatives, found from condition of equality
+            neighboring derivatives and from a given set of points (x, y)
+            for building up cubic polynomial in Hermite form.
+
+    Attributes
+    ----------
+    n : int
+        Number of given points.
+
+    Methods
+    -------
+    __call__
+    hermit_cubic_spline
+
+    """
     def __init__(self, x, y, der):
         self.x = x
         self.y = y
@@ -20,14 +37,18 @@ class cyclic_interp_curve:
         self.der = np.copy(der)  # vector of derivatives
 
     def __call__(self, xnew):
-        '''
+        """
         returns the value of the spline at 'xnew'
-        '''
+        """
         if self.x[0] > xnew or self.x[-1] < xnew:
             raise ValueError(f'xnew not in ({x[0]},{x[-1]})', xnew)
         return self.hermit_cubic_spline(xnew)
 
     def hermit_cubic_spline(self, t):
+        """
+        Build up spline function - cubic polynomial in Hermite form
+        and returns value of this spline in t.
+        """
         i = np.argmax(self.x > t) - 1
 
         xa = self.x[i]
@@ -36,6 +57,8 @@ class cyclic_interp_curve:
         yb = self.y[i + 1]
         sa = self.der[i]
         sb = self.der[i + 1]
+        if np.allclose(xb, xa, atol=accuracy):
+            raise ZeroDivisionError("Division by zero", xa, xb)
         h = xb - xa
         m = (yb - ya) / h
         p1 = yb * (t - xa) / h + ya * (xb - t) / h
@@ -116,6 +139,7 @@ def Sherman_Morrison_algorithm(a, b, c, r, alpha, beta):
 
     Returns solution to the system of linear equations with almost
     tridiagonal matrix.
+
     Parameters
     ----------
     a : np.array
@@ -143,7 +167,7 @@ def Sherman_Morrison_algorithm(a, b, c, r, alpha, beta):
     The minimum size of matrix of SLE is 3 because
     tridiagonal matrix can not be defined otherwise.
     """
-    if b.shape[0] < 1:
+    if b.shape[0] <= 2:
         raise ValueError("Matrix size is not enough to interpolate")
     if a.shape != c.shape or a.shape[0] + 1 != b.shape[0]:
         raise ValueError(f"Vectors a({a.shape[0]}), b({b.shape[0]}), c({c.shape[0]}) have incompatible sizes",
@@ -176,29 +200,55 @@ def Sherman_Morrison_algorithm(a, b, c, r, alpha, beta):
 
 
 def Thomas_algorithm(a, b, c, d):
-    '''
+    """
     Implementation of Thomas algorithm.
 
-    variables:
-        a, b, c, d - lower tridiagonal, main, upper tridiagonal vectors of a matrix and free vector (dtype=float)
-        ac, bc, cc, dc - copies of vectors a, b, c, d
+    Parameters
+    ----------
+    a : 1-D array, shape (m-1,)
+        Vector of elements x_{i,i-1} of a tridiagonal matrix.
+    b : 1-D array, shape (m,)
+        Main diagonal of a tridiagonal matrix.
+    c : 1-D array, shape (m-1,)
+        Vector of elements x_{i-1,i} of a tridiagonal matrix.
+    d : 1-D array, shape (n,)
+        Vector of constants terms of a system of linear equations.
 
+    Returns
+    -------
+    х : 1-D array
+        Solution to a matrix equation.
 
-    returns:
-         х - solution vector
+    Notes
+    -----
+    If original tridiagonal matrix has shape n x m, then
+    length of vector b is m as a main diagonal,
+    lengths of vectors a and c is m - 1,
+    and length of vector d is n.
 
-    '''
-
+    """
     ac, bc, cc, dc = np.copy(a), np.copy(b), np.copy(c), np.copy(d)
-    n = len(b)
+    if ac is None or bc is None or cc is None or dc is None:
+        raise Exception("Error of copying")
+    if a.shape != c.shape or a.shape[0] + 1 != b.shape[0]:
+        raise ValueError(f"Vectors a({a.shape[0]}), b({b.shape[0]}), c({c.shape[0]}) have incompatible sizes",
+                         a.shape, b.shape, c.shape)
+    n = b.shape[0]
+    if n <= 2:
+        raise ValueError("Matrix size is not enough to solve linear system")
 
     # stage 1
 
+    if np.allclose(bc[0], 0, atol=accuracy):
+        raise ZeroDivisionError("Division by zero", bc[0])
     beta = bc[0]
     bc[0] = 1
     cc[0] = cc[0] / beta
     dc[0] = dc[0] / beta
+
     for i in range(1, n - 1):
+        if np.allclose(bc[i], cc[i - 1] * ac[i - 1], atol=accuracy):
+            raise ZeroDivisionError("Division by zero", bc[i], cc[i - 1] * ac[i - 1])
         alpha = (bc[i] - cc[i - 1] * ac[i - 1])
         beta = ac[i - 1]
         ac[i - 1] -= ac[i - 1] * bc[i - 1]
@@ -207,6 +257,8 @@ def Thomas_algorithm(a, b, c, d):
         dc[i] = (dc[i] - dc[i - 1] * beta) / alpha
 
     i = n - 1
+    if np.allclose(bc[i], cc[i - 1] * ac[i - 1], atol=accuracy):
+        raise ZeroDivisionError("Division by zero", bc[i], cc[i - 1] * ac[i - 1])
     alpha = (bc[i] - cc[i - 1] * ac[i - 1])
     beta = ac[i - 1]
     ac[i - 1] -= ac[i - 1] * bc[i - 1]
